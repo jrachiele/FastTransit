b4c48178b42f5c82f1cf6a63c831e144a01db799
Jacob Rachiele <jrachiele@utexas.edu>
2016-03-19 15:19:22 -0500

    Quick test for GTFS message

--- src/main/java/com/voltbus/network/Response.java
+++ src/main/java/com/voltbus/network/Response.java
@@ -1,10 +1,12 @@
 package com.voltbus.network;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by jacob on 2/15/16.
  */
 public interface Response {
     int statusCode() throws IOException;
+    InputStream responseBody() throws IOException;
 }
--- src/main/java/com/voltbus/network/VoltHttpResponse.java
+++ src/main/java/com/voltbus/network/VoltHttpResponse.java
@@ -3,6 +3,7 @@ package com.voltbus.network;
 import org.springframework.http.client.ClientHttpResponse;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by jacob on 2/15/16.
@@ -17,5 +18,9 @@ public final class VoltHttpResponse implements Response {
 
     public int statusCode() throws IOException {
         return response.getRawStatusCode();
+    }
+
+    public InputStream responseBody() throws IOException {
+        return response.getBody();
     }
 }
